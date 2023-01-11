f529a7a7b017f67ed29ec817ce0a1ad892cc58a5
Divine <higormaria20@gmail.com>
2023-01-11 13:14:55 -0300

    Update Ex037.py

--- Ex037.py
+++ Ex037.py
@@ -1,5 +1,5 @@
 #Exercício 037 - Conversos de número decimal para binário, octal e hexadecimal.
-print("-=" * 20 + "-")
+print("\033[33m-=" * 20 + "-\033[m")
 print("Conversor de número decimal para binário, octal ou hexadecimal.")
 print("-=" * 20 + "-")
 num = int(input("Digite um número a ser convertido: "))
@@ -13,4 +13,4 @@ elif inf == 2:
     print(o.replace('0o', ""))
 elif inf == 3:
     h = hex(num)
-    print(h)
+    print(h.replace('0x', ""))
