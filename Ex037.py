6ab46134f1db31cbfc92e2e7258e63f2a7f0b385
Divine <higormaria20@gmail.com>
2023-01-11 14:41:18 -0300

    -

--- Ex037.py
+++ Ex037.py
@@ -1,16 +1,20 @@
 #Exercício 037 - Conversos de número decimal para binário, octal e hexadecimal.
 print("\033[33m-=" * 20 + "-\033[m")
-print("Conversor de número decimal para binário, octal ou hexadecimal.")
-print("-=" * 20 + "-")
+print("\033[34mConversor de número decimal para binário, octal ou hexadecimal.")
+print("\033[33m-=" * 20 + "-\033[m")
 num = int(input("Digite um número a ser convertido: "))
-print("Digite 1 para converter o número inserido para binário;\nDigite 2 para converter o número inserido para octal;\nDigite 3 para converter o número inserido para hexadecimal.")
+print("\033[32mDigite 1 para converter o número inserido para binário;\nDigite 2 para converter o número inserido para octal;\nDigite 3 para converter o número inserido para hexadecimal.\033[m")
 inf = int(input("Digite o número: "))
+print("\033[33m-=" * 20 + "-\033[m")
 if inf == 1:
-    b = bin(num)
-    print(b.replace('0b', ""))
+    b = bin(num).replace('0b', "")
+    print(f"Convertendo o número {num} em binário, temos: {b}")
 elif inf == 2:
-    o = oct(num)
-    print(o.replace('0o', ""))
+    o = oct(num).replace('0o', "")
+    print(f"Convertendo o número {num} em octal, temos: {o}")
 elif inf == 3:
-    h = hex(num)
-    print(h.replace('0x', ""))
+    h = hex(num).replace('0x', "")
+    print(f"Convertendo o número {num} em hexadecimal, temos: {h}")
+elif inf > 3:
+    print("O número inserido não corresponde a nenhuma opção acima. Reincie o programa.")
+print("\033[33m-=" * 20 + "-\033[m")
