6cef1de25c8aadf585b29d9669b69ee607d42758
Divine <higormaria20@gmail.com>
2023-01-11 09:26:15 -0300

    Create Ex036.py

---
+++ Ex036.py
@@ -0,0 +1,7 @@
+#Exercício 036 - Aprovando ou negando um empréstimo
+print("\033[1;33m-=" * 20 + "\033[1;33m-\033[m")
+print("\033[1;35mAvaliador de empréstimos.")
+print("\033[1;33m-=" * 20 + "\033[1;33m-\033[m")
+val = float(input("\033[32mPara começarmos a avaliar a possibilidade de um empréstimo, digite o valor da casa: \033[m"))
+sal = float(input("\033[32mAgora, digite o seu salário: \033[m"))
+ano = int(input("E "))
