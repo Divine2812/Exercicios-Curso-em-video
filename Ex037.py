793371811499afd1002837a700fb582df3a2505d
Divine <higormaria20@gmail.com>
2023-01-11 12:36:54 -0300

    Create Ex037.py

---
+++ Ex037.py
@@ -0,0 +1,16 @@
+#Exercício 037 - Conversos de número decimal para binário, octal e hexadecimal.
+print("-=" * 20 + "-")
+print("Conversor de número decimal para binário, octal ou hexadecimal.")
+print("-=" * 20 + "-")
+num = int(input("Digite um número a ser convertido: "))
+print("Digite 1 para converter o número inserido para binário;\nDigite 2 para converter o número inserido para octal;\nDigite 3 para converter o número inserido para hexadecimal.")
+inf = int(input("Digite o número: "))
+if inf == 1:
+    b = bin(num)
+    print(b.replace('0b', ""))
+elif inf == 2:
+    o = oct(num)
+    print(o.replace('0o', ""))
+elif inf == 3:
+    h = hex(num)
+    print(h)
