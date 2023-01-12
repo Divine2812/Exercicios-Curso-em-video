c4143c0855d492082acbb3ad2bbbc0cdbe570113
Divine <higormaria20@gmail.com>
2023-01-12 10:04:01 -0300

    Update Ex042.py

--- Ex042.py
+++ Ex042.py
@@ -4,10 +4,13 @@ l2 = float(input("Segundo Segmento: "))
 l3 = float(input("Terceiro Segmento: "))
 print("\033[33m=\033[m" * 40)
 if l1 + l2 <= l3 or l1 + l3 <= l2 or l2 + l3 <= l1:
-
-    print("As medidas inseridas \033[4;31mnão formam\033[m um triângulo! =(")
+    print("As medidas inseridas não formam um triângulo!")
 else:
-    print("As medidas inseridas \033[4;32mformam\033[m um triângulo. =D")
-
+    if l1 == l2 and l2 == l3:
+        eq = 'equilátero'
+    elif l1 == l2 or l1 == l2 or l3 == l2 or l3 == l1:
+        eq = "isósceles"
+    elif l1 != l2 and l2 != l3:
+        eq = "escaleno"
+    print(f"As medidas inseridas \033[4;32mformam\033[m um triângulo {eq}. =D")
 print("\033[33m=\033[m" * 40)
-#Exercício com cores
