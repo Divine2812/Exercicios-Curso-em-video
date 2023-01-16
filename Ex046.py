148c0201e9338e376432896b7833a63d6c413086
Divine <higormaria20@gmail.com>
2023-01-16 11:47:45 -0300

    Update Ex046.py

--- Ex046.py
+++ Ex046.py
@@ -1,4 +1,14 @@
-for c in range (0, 3):
-    s = int(input("num: "))
-print(s)
-print("terminemo")
+#Exercício 046 - Contagem regressiva de fogos.
+from time import sleep
+print('\033[33m-=' * 30 + '-\033[m')
+print('\033[35mContagem regressiva dos fogos.')
+print('\033[33m-=' * 30 + '-\033[m')
+n = str(input("Digite qualquer coisa para iniciar a contagem"))
+if type(n) == str:
+    for c in range (0, 10 , -1)
+    print(c)
+    sleep(1)
+
+#for c in range (0, 3):
+ #   s = int(input("num: "))
+  #  n += s
