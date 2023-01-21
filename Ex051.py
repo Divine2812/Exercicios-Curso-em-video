f1679ca9dd483d1bf578442f67e195e3359d4f4c
Divine <higormaria20@gmail.com>
2023-01-21 14:46:37 -0300

    Update Ex051.py

--- Ex051.py
+++ Ex051.py
@@ -4,5 +4,10 @@ print("\033[35mMostrar os 10 primeiros termos de uma P.A.")
 print('\033[33m-=' * 30 + '-\033[m')
 a1 = int(input("Digite o primeiro termo da P.A: "))
 r = int(input("Digite a razão da P.A: "))
-for c in range (0, 10, r):
-    print(c)
+pf = a1 + (10 - 1) * r + 1
+lis = []
+for c in range (a1, pf, r):
+    lis.append(c)
+sr = str(lis)
+print(f"Os 10 primeiros números da P.A inserida são: {str(lis)[1:-1]}")
+print('\033[33m-=' * 30 + '-\033[m')
