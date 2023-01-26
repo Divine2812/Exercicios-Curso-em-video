9d45636154d7a438cd85eadca52108881dc528b0
Divine <higormaria20@gmail.com>
2023-01-26 17:18:04 -0300

    Update Ex052.py

--- Ex052.py
+++ Ex052.py
@@ -1,14 +1,15 @@
 #Exercício 052 - Ver se o número inserido é primo ou não.
+from prime_test import prime
 print('\033[33m-=' * 30 + '-\033[m')
 print('\033[35mIdentificador de números primos.')
 print('\033[33m-=' * 30 + '-\033[m')
 n = int(input("Digite um número: "))
-for c in range (2, 100):
-    k = n % c
-if k > 0 and n / n == 1 and n / 1 == n or n == 2 or n == 3:
+prime_result = prime.test(n)
+if prime_result is True:
     print(f"O número {n} é um número primo.")
 else:
    print(f"O número {n} não é um número primo.")
 print('-=' * 30 + '-')
+
 
 
