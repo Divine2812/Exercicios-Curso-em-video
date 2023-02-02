f670955a4dee33f28ce7e76be4f34722874760e7
Divine <higormaria20@gmail.com>
2023-02-02 11:04:50 -0300

    Create Ex060.py

---
+++ Ex060.py
@@ -0,0 +1,21 @@
+#Exercício 060 - Calcular o fatorial do número inserido.
+from time import sleep
+print('\033[33m-=' * 20 + '-\033[m')
+print("\033[35m       Calculadora de fatorial")
+print('\033[33m-=' * 20 + '-\033[m')
+n = int(input("Digite um número a ser calculado: "))
+fatorial = n
+f = n
+sleep(1)
+print(f"Calculando o fatorial do número {n}...")
+sleep(1)
+while fatorial > 0:
+    print(fatorial, end="")
+    print(' x ' if fatorial > 1 else ' = ', end='')
+    fatorial -= 1
+    if fatorial > 0:
+        f *= fatorial    
+print(f)
+
+    
+    
