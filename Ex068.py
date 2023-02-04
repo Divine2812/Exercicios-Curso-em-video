b813fc820c7067300b886e90d3e20e0dfa6b827e
Divine <higormaria20@gmail.com>
2023-02-04 17:11:15 -0300

    Update Ex068.py

--- Ex068.py
+++ Ex068.py
@@ -5,6 +5,7 @@ print('\033[33m-=' * 30 + '-\033[m')
 import random
 computador = random.randint(0, 10)
 parouimpar = 0
+contador = 0
 while True:
     parouimpar = int(input("\033[34mDigite [1] para ÍMPAR;\nDigite [2] para PAR;\nDigite a sua escolha:\033[m "))   
     while parouimpar > 2 or parouimpar < 1:
@@ -18,10 +19,15 @@ while True:
     soma = computador + jogador
     if soma % 2 == 0 and parouimpar == 2:
         print(f"\033[32mPAR!! Você ganhou. O computador jogou {computador} e a soma dos números deu {soma}.\033[m ")
+        contador += 1
+        print(f"\033[34mTotal de {contador} vitórias.\033m")
     elif soma % 3 == 0 and parouimpar == 1:
-         print(f"\033[32mÍMPAR!! Você ganhou. O computador jogou {computador} e a soma dos números deu {soma}.\033[m ")
+         print(f"\033[32mÍMPAR!! Você ganhou. O computador jogou {computador} e a soma dos números deu {soma}.\033[m")
+         contador += 1
+         print(f"\033[34mTotal de {contador} vitórias.\033m")
     else:
         print(f"\033[31mVocê escolheu {parouimparnome}. O computador jogou {computador} e a soma dos números deu {soma}. Você perdeu :/\033[m")
+        print(f"\033[34mTotal de {contador} vitórias.\033m")
         print('\033[33m-=' * 30 + '-\033[m')
         break
     print('\033[33m-=' * 30 + '-\033[m')
